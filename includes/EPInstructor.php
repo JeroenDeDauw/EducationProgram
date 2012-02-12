1ddab15e16d2f036fcbb102a01fdf093457d4071
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2012-02-12 16:14:40 +0000

    follow up to recent schema changes

--- includes/EPInstructor.php
+++ includes/EPInstructor.php
@@ -14,121 +14,34 @@
 class EPInstructor extends EPRoleObject implements EPIRole {
 
 	/**
-	 * Field for caching the linked user.
+	 * (non-PHPdoc)
+	 * @see DBDataObject::getDBTable()
+	 */
+	public static function getDBTable() {
+		return 'ep_instructors';
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see DBDataObject::getFieldPrefix()
+	 */
+	public static function getFieldPrefix() {
+		return 'instructor_';
+	}
+	
+	/**
+	 * @see parent::getFieldTypes
 	 *
 	 * @since 0.1
-	 * @var User|false
+	 *
+	 * @return array
 	 */
-	protected $user = false;
+	protected static function getFieldTypes() {
+		return array(
+			'id' => 'id',
 
-	/**
-	 * Holds the user id if only an id was provided.
-	 * 
-	 * @since 0.1
-	 * @var integer
-	 */
-	protected $userId;
-	
-	/**
-	 * Create a new instructor object from a user id.
-	 * 
-	 * @since 0.1
-	 * 
-	 * @param integer $userId
-	 * @param null|array|string $fields
-	 * 
-	 * @return EPInstructor
-	 */
-	public static function newFromUserId( $userId, $fields = null ) {
-		return new self( $userId );
-	}
-	
-	/**
-	 * Create a new instructor object from a User object.
-	 * 
-	 * @since 0.1
-	 * 
-	 * @param User $user
-	 * @param null|array|string $fields
-	 * 
-	 * @return EPInstructor
-	 */
-	public static function newFromUser( User $user, $fields = null ) {
-		return new self( $user );
-	}
-	
-	/**
-	 * Constructor.
-	 * 
-	 * @since 0.1
-	 * 
-	 * @param User|integer $user
-	 */
-	public function __construct( $user ) {
-		if ( is_integer( $user ) ) {
-			$this->userId = $user;
-		}
-		elseif ( $user instanceof User ) {
-			$this->user = $user;
-		}
-		else {
-			throw new MWException( 'Instructors can only be constructed from an user id or an User object.' );
-		}
-	}
-	
-	/**
-	 * Returns the user that this instructor is.
-	 * 
-	 * @since 0.1
-	 * 
-	 * @return User
-	 */
-	public function getUser() {
-		if ( $this->user === false ) {
-			$this->user = User::newFromId( $this->userId );
-		}
-		
-		return $this->user;
-	}
-	
-	/**
-	 * Returns the name of the instroctor, using their real name when available.
-	 * 
-	 * @since 0.1
-	 * 
-	 * @return string
-	 */
-	public function getName() {
-		return $this->getUser()->getRealName() === '' ? $this->getUser()->getName() : $this->getUser()->getRealName();
-	}
-	
-	/**
-	 * Retruns the user link for this instructor, using their real name when available.
-	 * 
-	 * @since 0.1
-	 * 
-	 * @return string
-	 */
-	public function getUserLink() {
-		return Linker::userLink(
-			$this->getUser()->getId(),
-			$this->getUser()->getName(),
-			$this->getName()
+			'user_id' => 'int',
 		);
-	}
-	
-	/**
-	 * Returns the tool links for this mentor.
-	 * 
-	 * @since 0.1
-	 * 
-	 * @param IContextSource $context
-	 * @param EPCourse|null $course
-	 * 
-	 * @return string
-	 */
-	public function getToolLinks( IContextSource $context, EPCourse $course = null ) {
-		return EPUtils::getRoleToolLinks( $this, $context, $course );
 	}
 	
 	/**
@@ -139,23 +52,4 @@ class EPInstructor extends EPRoleObject implements EPIRole {
 		return 'instructor';
 	}
 
-	/**
-	 * Returns the courses this campus ambassdor is associated with.
-	 *
-	 * @since 0.1
-	 *
-	 * @param string|array|null $fields
-	 * @param array $conditions
-	 *
-	 * @return array of EPCourse
-	 */
-	protected function doGetCourses( $fields, array $conditions ) {
-		return EPUtils::getCoursesForUser(
-			$fields,
-			$this->getField( 'user_id' ),
-			EP_INSTRUCTOR,
-			$conditions
-		);
-	}
-	
 }
