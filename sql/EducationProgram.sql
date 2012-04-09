cac8fef984463decae70bd8dc77f309f8954b30c
jeroendedauw <jeroendedauw@gmail.com>
2012-04-09 15:33:54 +0200

    add events table
    Change-Id: Ieef2eee9bf87ac53c2f5dc4c233095814736a320

--- sql/EducationProgram.sql
+++ sql/EducationProgram.sql
@@ -175,6 +175,23 @@ CREATE INDEX /*i*/ep_oas_visible ON /*_*/ep_oas (oa_visible);
 
 
 
+-- Education timeline events.
+CREATE TABLE IF NOT EXISTS /*_*/ep_events (
+  event_id                   INT unsigned        NOT NULL auto_increment PRIMARY KEY,
+  event_course_id            INT unsigned        NOT NULL, -- Foreign key on ep_courses.course_id
+  event_user_id              INT unsigned        NOT NULL, -- The user creating the event. Foreign key on user.user_id
+  event_time                 varbinary(14)       NOT NULL, -- Time the event took place
+  event_type                 TINYINT unsigned    NOT NULL, -- Type of the event
+  event_info                 BLOB                NOT NULL -- Event info, can be different fields depending on event type
+) /*$wgORMTableOptions*/;
+
+CREATE INDEX /*i*/ep_events_course_id ON /*_*/ep_events (event_course_id);
+CREATE INDEX /*i*/ep_events_user_id ON /*_*/ep_events (event_user_id);
+CREATE INDEX /*i*/ep_events_time ON /*_*/ep_events (event_time);
+CREATE INDEX /*i*/ep_events_type ON /*_*/ep_events (event_type);
+
+
+
 -- Revision table, holding blobs of various types of objects, such as orgs or students.
 -- This is somewhat based on the (core) revision table and is meant to serve
 -- as a prototype for a more general system to store this kind of data in a visioned fashion.
