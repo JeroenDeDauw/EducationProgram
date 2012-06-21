601f05749771bea9cea0018d95f9c6dd5eeaa823
Reedy <reedy@wikimedia.org>
2012-06-21 23:21:51 +0100

    Define NS' if not already defined (stops errors in LU etc)
    Change-Id: I76fdaad713fe2bc2302e44eea39d4191ee3a354f

--- EducationProgram.i18n.ns.php
+++ EducationProgram.i18n.ns.php
@@ -12,11 +12,18 @@
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
 
+if ( !defined( 'EP_NS_COURSE' ) ) {
+	define( 'EP_NS_COURSE',				442 + 0 );
+	define( 'EP_NS_COURSE_TALK', 		442 + 1 );
+	define( 'EP_NS_INSTITUTION', 		442 + 2 );
+	define( 'EP_NS_INSTITUTION_TALK', 	442 + 3 );
+}
+
 $namespaceNames = array();
 
 $namespaceNames['en'] = array(
-	/*EP_NS_COURSE*/ 442 + 0     	  	=> 'Course',
-	/*EP_NS_COURSE_TALK*/ 442 + 1  		=> 'Course_talk',
-	/*EP_NS_INSTITUTION*/ 442 + 2       => 'Institution',
-	/*EP_NS_INSTITUTION_TALK*/ 442 + 3  => 'Institution_talk',
+	EP_NS_COURSE		  	=> 'Course',
+	EP_NS_COURSE_TALK  		=> 'Course_talk',
+	EP_NS_INSTITUTION		=> 'Institution',
+	EP_NS_INSTITUTION_TALK  => 'Institution_talk',
 );
