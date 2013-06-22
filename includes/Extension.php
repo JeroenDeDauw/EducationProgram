afa63f5f7b16873e237a52fd03b0eaa0cc4b1ad3
jeroendedauw <jeroendedauw@gmail.com>
2013-06-22 16:26:50 +0200

    Remove Settings::newFromGlobals (fixed version)
    This is a fixed version of change I29340093b7141b10498d2d1eaffb8125d6c9a224.
    
    Change-Id: Idcac47914c001c426049f6bf54e3297390b44380

--- includes/Extension.php
+++ includes/Extension.php
@@ -110,7 +110,7 @@ class Extension {
 		static $instance = null;
 
 		if ( $instance === null ) {
-			$instance = new static( Settings::newFromGlobals( $GLOBALS ) );
+			$instance = new static( new Settings( $GLOBALS['egEPSettings'] ) );
 		}
 
 		return $instance;
