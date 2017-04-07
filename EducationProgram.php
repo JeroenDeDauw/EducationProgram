85e4f223ceef6f43eeda9a35da2d824759795bae
Reedy <reedy@wikimedia.org>
2017-04-07 23:10:17 +0100

    Load ExtensionMessageFiles in PHP entry point
    Bug: T162481
    Change-Id: Ied56e5419348f4a5de0651150c708363b5d1cbcf

--- EducationProgram.php
+++ EducationProgram.php
@@ -3,6 +3,8 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'EducationProgram' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['EducationProgram'] = __DIR__ . '/i18n';
+	$wgExtensionMessagesFiles['EducationProgramAlias'] = __DIR__ . '/EducationProgram.i18n.alias.php';
+	$wgExtensionMessagesFiles['EPNamespaces'] = __DIR__ . '/EducationProgram.i18n.ns.php';
 	/*wfWarn(
 		'Deprecated PHP entry point used for EducationProgram extension. ' .
 		'Please use wfLoadExtension instead, ' .
